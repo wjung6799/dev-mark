9f25f7df71402c823df48ee11a5d85850c52ab3e
Won Jung <wjung6799@outlook.com>
2026-03-17 07:06:39 -0700

    adding git ignore and stuff

--- dist/utils/storage.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function ensureDiaryDir(projectPath: string): string;
-export declare function writeEntry(projectPath: string, content: string): string;
-export declare function readEntries(projectPath: string, count: number): string[];
